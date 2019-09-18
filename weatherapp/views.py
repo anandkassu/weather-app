12dd61a70e8fdbf8a81c0b678665f3da4dd3a300
anandkassu00 <anandkassu00@gmail.com>
2019-09-18 15:37:21 +0530

    date added

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -60,7 +60,7 @@ def index(request):
         'message_class' : message_class
     }
 
-    return render(request, 'weather/weather.html', context)
+    return render(request, 'weather.html', context)
 
 def delete_city(request, city_name):
     City.objects.get(name=city_name).delete()
